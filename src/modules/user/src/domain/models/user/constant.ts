690307cf484488fc3ebcd534dc2eb2e928882bab
TJ <132363388+TJYumul@users.noreply.github.com>
2025-02-12 15:54:57 +0800

    Import prisma

--- src/modules/user/src/domain/models/user/constant.ts
+++ src/modules/user/src/domain/models/user/constant.ts
@@ -1,31 +1,4 @@
-import type { Role } from "@prisma/client";
+import type { Prisma, User } from "@prisma/client";
 
-export type IUserRawObject = {
-  id: string;
-  username: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  isSuperAdmin: boolean;
-  role: Role;
-  isDeleted: boolean;
-  deletedAt: Date | null;
-  createdAt: Date;
-  updatedAt: Date;
-};
-
-export type IUserSchema = {
-  id: string;
-  username: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  isSuperAdmin: boolean;
-  role: Role;
-  isDeleted: boolean;
-  deletedAt: Date | null;
-  createdAt: Date;
-  updatedAt: Date;
-};
+export type IUserRawObject = User;
+export type IUserSchema = Prisma.UserUncheckedCreateInput;
